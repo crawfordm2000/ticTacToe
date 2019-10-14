05bf10a9581a1d2b2e37488cdbb8d9277c0db77e
Matthew Crawford <crawfordm2000@gmail.com>
2019-10-14 15:32:41 -0400

    fixed bug

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 const tiles = document.querySelectorAll('.box');// array divs
 const gameBoard = document.querySelector('.container');//
-
+let counter = 0;
 let moves = []
 
 //console.log(gameBoard);
 
-tiles.forEach(function(i , y){
+tiles.forEach(function(tile , y){
     
 
     let play = document.createElement('p');
     
 
-    i.addEventListener('click', function(e){
+    tile.addEventListener('click', function(e){
 
-        if(y % 2 == 0){
-            console.log(this)
-            moves[this.id - 1] = 'X'
-            console.log(moves)
-            
-            play.innerText = "X";
-            i.appendChild(play);
+        if(counter % 2 == 0){
+            if(!play.innerText){
+                //console.log(this)
+                //moves[this.id - 1] = 'X'
+                //console.log(moves)
+                play.innerText = "X";
+                tile.appendChild(play);
+                counter++;
+            } 
         } else {
+            if(!play.innerText){
+                play.innerText = "O";
+                tile.appendChild(play);
+                counter++;
+            }
             
-            play.innerText = "O";
-            i.appendChild(play);
         }
 
         console.log(play.innerText);
         
     })
     })
-
-    
-    
-
-    
