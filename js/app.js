e05a4841987aef1f8f24b5f6d55a8ab8f0811000
Matthew Crawford <crawfordm2000@gmail.com>
2019-10-15 16:09:18 -0400

    fixed reset button

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const tiles = document.querySelectorAll('.box');// array divs
 const gameBoard = document.querySelector('.container');//
-let counter = 0;
-let moves = [];
+let counter = 0; // used to determine whether an x or o is placed in the div
+let moves = []; // this array captures all the values of the divs to check the win conditions
 let button = document.querySelector('.btn');
 let background = document.querySelector('.container');
+
 const winCombos = [
     [0,1,2],
     [3,4,5],
@@ -43,29 +44,32 @@ let winCheck = function(){
 // determines whether an X or O is added to the element with the event listener
 let gameStart = function(){
 
-    //changes background color of gameboard
+    //changes background color of gameboard when the play button is pressed
     background.style.backgroundColor = "#f4f4f4";
     // sets text of button to reset
     button.innerText = "Reset";
-    //code for the reset button to reset the gameboard
+
+
+    //code for the reset button to reset the gameboard after or during play
     button.addEventListener('click', function(){
-        for(let i = 0; i < moves.length; i++){
-            tiles[i].innerText = "";
-            console.log('you got me');
-            console.log(moves[i]);
+    for(let i = 0; i < moves.length; i++){
+        tiles[i].innerText = null;
         }
+        moves = [];
     })
+
+
     
     // to loop through the 9 divs and apply event listeners to them
     tiles.forEach(function(tile , index){
         
-        
+        // adds eventListeners to each div 
         tile.addEventListener('click', function(e){
-
+            // condition determines whether an x or o is added to the div
             if(counter % 2 == 0){
-                if(!tile.innerText){
-                    moves[this.id] = 'X';
-                    tile.innerText = 'X'
+                if(!tile.innerText){ // code only runs if the innerText = '' (undefined)
+                    moves[this.id] = 'X'; // this == the div clicked. '.id' refers to that divs id
+                    tile.innerText = 'X' // sets innerText for the div clicked
                     counter++;
                 } 
             } else {
@@ -85,4 +89,10 @@ let gameStart = function(){
 
 
 button.addEventListener('click', gameStart);
+
+
+
+
+
+
 
