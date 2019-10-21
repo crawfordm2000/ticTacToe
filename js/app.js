57506462e06fef20bbb5a4065cc112ee58546815
Matthew Crawford <crawfordm2000@gmail.com>
2019-10-21 16:52:54 -0400

    paired down win conditions

--- js/app.js
+++ js/app.js
@@ -2,14 +2,11 @@
 
 const tiles = document.querySelectorAll('.box');// array of divs in my html 
 let counter = 0; // used to determine whether an x or o is placed in the div
-let moves = []; // this array captures all the values of the divs allows me to check the win conditions
+let moves = []; // this array captures all the values of the divs and allows me to check the win conditions
 const button = document.querySelector('.btn');
 const green = "#1ec20c";
 const red = "#d41e1e";
-const slider = document.querySelector('.slider');
 const background = document.querySelector('.container');
-
-
 
 /********************* FUNCTIONS *********************/
 
@@ -27,6 +24,8 @@ let winColor = (box1, box2, box3) => {
     } else if (moves[box1] == "O"){
         color(box1, red); color(box2, red); color(box3, red);
     }
+    disableClicks();
+    resetButton();
 }
 
 // enables the player to make a game decision. 
@@ -78,7 +77,7 @@ const randomChoice = () => {
         } else if(moves[x] !== undefined && counter <= 8 && winCheck() === false){
             randomChoice();   
         }
-    }, 150);    
+    }, 170);    
 }
 
 // sets the player choice inside an array. Sets inner text to "x"
@@ -92,48 +91,42 @@ const onClick = (event) => {
     } 
 }
 
-// Will be used to add further functionality in the future.
-let winner = () => {
-    if(counter === 9){
-        console.log('draw');
-    } else if(counter != 9 && counter % 2 === 0){
-        console.log('Player 2 Wins');
-    } else if(counter != 9 && counter % 2 !== 0){
-        console.log('Player 1 Wins');
+
+//This is the condition that must be met for a win to occur. 
+let winCond = (box1, box2, box3) => {
+    if (moves[box1] != undefined && moves[box1] === moves[box2] && moves[box1] === moves[box3]){
+        return true;
+    } else{
+        return false;
     }
-    disableClicks();
-    resetButton();
 }
-  
+
 
 //Checks win conditions and disables the game board by calling winner, then calls the color function
 let winCheck = () => {
-    if(moves[0] != undefined && moves[0] === moves[1] && moves[0] === moves[2]){
-        winner(); winColor(0,1,2);
-    } else if (moves[3] != undefined && moves[3] === moves[4] && moves[5] === moves[3]){
-        winner(); winColor(3,4,5);
-    } else if (moves[6] != undefined && moves[6] === moves[7] && moves[8] === moves[6]){
-        winner(); winColor(6,7,8);
-    } else if (moves[0] != undefined && moves[0] === moves[4] && moves[8] === moves[0]){
-        winner(); winColor(0,4,8);
-    } else if (moves[2] != undefined && moves[2] === moves[4] && moves[6] === moves[2]){
-        winner(); winColor(2,4,6);   
-    } else if (moves[0] != undefined && moves[0] === moves[3] && moves[6] === moves[0]){
-        winner(); winColor(0,3,6);
-    } else if (moves[1] != undefined && moves[1] === moves[4] && moves[7] === moves[1]){
-        winner(); winColor(1,4,7);
-    } else if (moves[2] != undefined && moves[2] === moves[5] && moves[8] === moves[2]){
-        winner(); winColor(2,5,8);
+    if(winCond (0,1,2) === true){
+        winColor(0,1,2);
+    } else if (winCond(3,4,5) === true){
+        winColor(3,4,5);
+    } else if (winCond (6,7,8) === true){
+        winColor(6,7,8);
+    } else if (winCond(0,4,8) === true){
+         winColor(0,4,8);
+    } else if (winCond(2,4,6) === true){
+         winColor(2,4,6);   
+    } else if (winCond(0,3,6) === true){
+         winColor(0,3,6);
+    } else if (winCond(1,4,7) === true){
+         winColor(1,4,7);
+    } else if (winCond(2,5,8) === true){
+         winColor(2,5,8);
     } else if (counter === 9){
-        tiles.forEach(tiles => tiles.style.color = "grey");
-        winner();
+        tiles.forEach(tiles => tiles.style.color = "grey");   
     } else {
         return false;
     }
-
 }
-
-// after clicking the start button, a game will begin.    
+   
 startButton();
 
 
